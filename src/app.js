61968a5f8702fd9a28c8a02667e8a9c83dcb0af4
KevinSinatria <kevinsinatria8@gmail.com>
2025-09-14 14:29:21 +0700

    feat(personnel): upload image to drive completed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import cors from "cors";
 import routes from "./routes/index.route.js";
 
 const app = express();
---
+++ src/services/personnel.service.js
@@ -0,0 +1,42 @@
+import * as PersonnelRepository from "../repositories/personnel.repository.js";
+import { uploadFileToGoogleDrive } from "../utils/googleDrive.js";
+
+const personnelFolderId = process.env.GOOGLE_DRIVE_PERSONNEL_FOLDER_ID;
+
+export const getAllPersonnel = async () => {
+	try {
+		return await PersonnelRepository.findAll();
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+};
+
+export const createPersonnel = async (dataBody, file) => {
+	try {
+		const uniqueFileName = `personnel-${Date.now()}-${Math.round(
+			Math.random() * 1e9
+		)}`;
+		const uploadedFile = uploadFileToGoogleDrive(
+			file,
+			uniqueFileName,
+			personnelFolderId
+		);
+
+		const personnelData = {
+			name: dataBody.name,
+			image_url: `https://drive.google.com/uc?id=${uploadedFile.id}`,
+			active: Boolean(dataBody.active),
+			// personnel_role: {
+			//    role_id: Number(dataBody.role_id),
+			//    subjects: dataBody.subjects,
+			//    position: dataBody.position,
+			// },
+		};
+
+		return await PersonnelRepository.create(personnelData);
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+};
